68bec69f1bac67ece754e07ed1581e3bedfa6126
beginninggrace <tldms123455@naver.com>
2025-05-04 18:48:14 +0900

    :sparkles: first commit

---
+++ src/main/java/com/three/recipingbackofficeservicebe/RecipingBackofficeServiceBeApplication.java
@@ -0,0 +1,13 @@
+package com.three.recipingbackofficeservicebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipingBackofficeServiceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipingBackofficeServiceBeApplication.class, args);
+    }
+
+}
