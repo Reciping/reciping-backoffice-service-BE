0ecc656d14e7a24ae0339300b277aa3f4b7b2f9b
Sanha Kang(Paradoxlover9417) <tksgk853@gmail.com>
2025-05-27 18:05:35 +0900

    :sparkles: feat : security, jwt 추가

---
+++ src/main/java/com/three/recipingbackofficeservicebe/global/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.three.recipingbackofficeservicebe.global.config;
+
+import com.three.recipingbackofficeservicebe.global.security.JwtOptionalFilter;
+import com.three.recipingbackofficeservicebe.global.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtOptionalFilter jwtOptionalFilter = new JwtOptionalFilter(jwtUtil);
+
+        http
+//                .cors(cors -> {})
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/v1/recipes/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/recipes/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(jwtOptionalFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
